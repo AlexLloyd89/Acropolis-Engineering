ad52ccabb808f66a74cb2350719dbcc62118209b
AlexLloyd89 <42323618+AlexLloyd89@users.noreply.github.com>
2019-01-13 13:39:13 -0500

    Initial Commit

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,19 @@
+      // Get the current year for the copyright
+      $("#year").text(new Date().getFullYear());
+
+      //ScrollSpy
+      $('body').scrollspy({target: '#main-nav'});
+
+      $('#main-nav a').on('click', function(e){
+        if (this.hash !== ''){
+          e.preventDefault();
+
+          const hash = this.hash;
+
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top}, 800, function(){
+              window.location.hash = hash;
+            });
+          }
+        
+      });
