19d07144d11bd917d8ea5c3cb033929d216ba3bc
AlexLloyd89 <42323618+AlexLloyd89@users.noreply.github.com>
2019-01-18 10:32:43 -0500

    Add files via upload

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,19 +1,19 @@
-     //ScrollSpy
-      $('body').scrollspy({target: '#main-nav'});
+    //ScrollSpy
+    $('body').scrollspy({target: '#main-nav'});
 
-      $('#main-nav a').on('click', function(e){
-        if (this.hash !== ''){
-          e.preventDefault();
+    $('#main-nav a').on('click', function(e){
+      if (this.hash !== ''){
+        e.preventDefault();
 
-          const hash = this.hash;
+        const hash = this.hash;
 
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top}, 800, function(){
-              window.location.hash = hash;
-            });
-          }
-        
-      });
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top}, 800, function(){
+            window.location.hash = hash;
+          });
+        }
+      
+    });
 
- // Get the current year for the copyright
-      $("#year").text(new Date().getFullYear());
+// Get the current year for the copyright
+    $("#year").text(new Date().getFullYear());
